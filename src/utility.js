1fb50dc2074e2cd82e84e4042ed511b36bbcf8b1
Rowel Capulong <capulong.rowel@gmail.com>
2024-01-25 02:29:41 +0800

    ff

--- src/utility.js
+++ src/utility.js
@@ -4,7 +4,6 @@ export async function isAuthenticated(){
     try{
         const res = await fetch('https://user-authentication-server.vercel.app/verify-authentication', {
             method:'GET',
-            credentials: 'include',
             headers: {'content-type': 'application/json'}
         });
 
